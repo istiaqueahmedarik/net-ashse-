7c0be6797ac0ef4239d6595757d6c6148b09b11b
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-09-06 21:17:03 +0600

    aaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import { ConnectivityChecker } from "@/components/connectivity-checker";
 import Image from "next/image";
 
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 export default function Home() {
 
   return (
-    <ConnectivityChecker />
+    <Suspense fallback={<div>Loading...</div>}>
+      <ConnectivityChecker />
+    </Suspense>
   );
 }
