f15ae1c99cd2cf7c553c3c2073ef5e044f23a122
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-09-06 21:15:32 +0600

    aaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+'use client'
 import { ConnectivityChecker } from "@/components/connectivity-checker";
 import Image from "next/image";
 
+import dynamic from "next/dynamic";
+
 export default function Home() {
+
   return (
     <ConnectivityChecker />
   );
